4a32e29c18c4d77bc833b623f5d633bb734efa50
DLSUAdrielAmoguis <60635460+DLSUAdrielAmoguis@users.noreply.github.com>
2020-08-06 10:36:45 +0800

    Implemented the CPU timer and added driver templates

--- driver.c
+++ driver.c
@@ -1,10 +1,16 @@
 // Standard Library Imports
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 // Algorithms
 #include "GenerateData.c"
 #include "bubble.c"
+//#include "insertion.c"
+//#include "selection.c"
+//#include "merge.c"
+//#include "algo5.c"
+//#include "algo6.c"
 
 // Macros
 #define DATA_LEN 2500
@@ -35,6 +41,10 @@ int main()
     int counter;
     int i;
     char cChoice;
+    struct timespec begin, end;
+    int seconds;
+    long long unsigned int nanoseconds;
+    double timeElapsed;
 
     printf("Enter n: ");
     scanf("%d", &dataSize);
@@ -52,8 +62,11 @@ int main()
 
     // Bubble Sort Testing
     dataCopy = createArrCopy(testArr, dataSize);
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &begin);
+    printf("\n===== BUBBLE SORT START =====\n");
     counter = bubbleSort(dataCopy, dataSize);
-    printf("\n===== BUBBLE SORT =====\n");
+    printf("\n===== BUBBLE SORT END =====\n");
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
     printf("Print sorted array? [y/N]: ");
     scanf(" %c", &cChoice);
     if(cChoice == 'y' || cChoice == 'Y')
@@ -62,9 +75,124 @@ int main()
         printArrayData(dataCopy, dataSize);
     }
     printf("\nCounter: %d\n", counter);
-    printf("Time Elapsed: %d\n", 0);
+    seconds = end.tv_sec - begin.tv_sec;
+    nanoseconds = end.tv_nsec - begin.tv_nsec;
+    timeElapsed = seconds + nanoseconds*1e-9;
+    printf("Machine Execution Time: %lf seconds (%lf miliseconds)\n", timeElapsed, timeElapsed * 1000);
     counter = 0;
     free(dataCopy);
+
+    /*
+    // Insertion Sort Testing
+    dataCopy = createArrCopy(testArr, dataSize);
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &begin);
+    printf("\n===== INSERTION SORT START =====\n");
+    //counter = bubbleSort(dataCopy, dataSize);         CALL INSERTION SORT HERE
+    printf("\n===== INSERTION SORT END =====\n");
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
+    printf("Print sorted array? [y/N]: ");
+    scanf(" %c", &cChoice);
+    if(cChoice == 'y' || cChoice == 'Y')
+    {
+        printf("Sorted Array Elements:\n");
+        printArrayData(dataCopy, dataSize);
+    }
+    printf("\nCounter: %d\n", counter);
+    seconds = end.tv_sec - begin.tv_sec;
+    nanoseconds = end.tv_nsec - begin.tv_nsec;
+    timeElapsed = seconds + nanoseconds*1e-9;
+    printf("Machine Execution Time: %lf seconds (%lf miliseconds)\n", timeElapsed, timeElapsed * 1000);
+    counter = 0;
+    free(dataCopy);
+
+    // Selection Sort Testing
+    dataCopy = createArrCopy(testArr, dataSize);
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &begin);
+    printf("\n===== SELECTION SORT START =====\n");
+    //counter = bubbleSort(dataCopy, dataSize);         CALL SELECTION SORT HERE
+    printf("\n===== SELECTION SORT END =====\n");
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
+    printf("Print sorted array? [y/N]: ");
+    scanf(" %c", &cChoice);
+    if(cChoice == 'y' || cChoice == 'Y')
+    {
+        printf("Sorted Array Elements:\n");
+        printArrayData(dataCopy, dataSize);
+    }
+    printf("\nCounter: %d\n", counter);
+    seconds = end.tv_sec - begin.tv_sec;
+    nanoseconds = end.tv_nsec - begin.tv_nsec;
+    timeElapsed = seconds + nanoseconds*1e-9;
+    printf("Machine Execution Time: %lf seconds (%lf miliseconds)\n", timeElapsed, timeElapsed * 1000);
+    counter = 0;
+    free(dataCopy);
+
+    // Merge Sort Testing
+    dataCopy = createArrCopy(testArr, dataSize);
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &begin);
+    printf("\n===== MERGE SORT START =====\n");
+    //counter = bubbleSort(dataCopy, dataSize);         CALL MERGE SORT HERE
+    printf("\n===== MERGE SORT END =====\n");
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
+    printf("Print sorted array? [y/N]: ");
+    scanf(" %c", &cChoice);
+    if(cChoice == 'y' || cChoice == 'Y')
+    {
+        printf("Sorted Array Elements:\n");
+        printArrayData(dataCopy, dataSize);
+    }
+    printf("\nCounter: %d\n", counter);
+    seconds = end.tv_sec - begin.tv_sec;
+    nanoseconds = end.tv_nsec - begin.tv_nsec;
+    timeElapsed = seconds + nanoseconds*1e-9;
+    printf("Machine Execution Time: %lf seconds (%lf miliseconds)\n", timeElapsed, timeElapsed * 1000);
+    counter = 0;
+    free(dataCopy);
+
+    // Algo #5 Testing
+    dataCopy = createArrCopy(testArr, dataSize);
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &begin);
+    printf("\n===== ALGO5 SORT START =====\n");
+    //counter = bubbleSort(dataCopy, dataSize);         CALL ALGO #5 HERE
+    printf("\n===== ALGO5 SORT END =====\n");
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
+    printf("Print sorted array? [y/N]: ");
+    scanf(" %c", &cChoice);
+    if(cChoice == 'y' || cChoice == 'Y')
+    {
+        printf("Sorted Array Elements:\n");
+        printArrayData(dataCopy, dataSize);
+    }
+    printf("\nCounter: %d\n", counter);
+    seconds = end.tv_sec - begin.tv_sec;
+    nanoseconds = end.tv_nsec - begin.tv_nsec;
+    timeElapsed = seconds + nanoseconds*1e-9;
+    printf("Machine Execution Time: %lf seconds (%lf miliseconds)\n", timeElapsed, timeElapsed * 1000);
+    counter = 0;
+    free(dataCopy);
+
+    // Algo #6 Testing
+    dataCopy = createArrCopy(testArr, dataSize);
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &begin);
+    printf("\n===== ALGO6 SORT START =====\n");
+    //counter = bubbleSort(dataCopy, dataSize);         CALL ALGO #6 HERE
+    printf("\n===== ALGO6 SORT END =====\n");
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
+    printf("Print sorted array? [y/N]: ");
+    scanf(" %c", &cChoice);
+    if(cChoice == 'y' || cChoice == 'Y')
+    {
+        printf("Sorted Array Elements:\n");
+        printArrayData(dataCopy, dataSize);
+    }
+    printf("\nCounter: %d\n", counter);
+    seconds = end.tv_sec - begin.tv_sec;
+    nanoseconds = end.tv_nsec - begin.tv_nsec;
+    timeElapsed = seconds + nanoseconds*1e-9;
+    printf("Machine Execution Time: %lf seconds (%lf miliseconds)\n", timeElapsed, timeElapsed * 1000);
+    counter = 0;
+    free(dataCopy);
+    */
 
     system("PAUSE");
 
