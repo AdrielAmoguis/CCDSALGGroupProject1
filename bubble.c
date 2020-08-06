0441194e36d62a22bbcf19054f43b1519a563156
Dev-Acceus <59037332+Dev-Acceus@users.noreply.github.com>
2020-08-06 09:25:15 +0800

    Update bubble.c
    added counter variable

--- bubble.c
+++ bubble.c
@@ -14,13 +14,15 @@ void swap(int *xp, int *yp)
 // An optimized version of Bubble Sort 
 void bubbleSort(int arr[], int n) 
 { 
-   int i, j; 
+   int i, j, counter = 0;
    bool swapped; 
    for (i = 0; i < n-1; i++) 
    { 
+     counter += 1;				// counter variable here
      swapped = false; 
      for (j = 0; j < n-i-1; j++) 
      { 
+	counter += 1;				// counter variable here
         if (arr[j] > arr[j+1]) 
         { 
            swap(&arr[j], &arr[j+1]); 
