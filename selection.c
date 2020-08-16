fcb2b3c918078bcac4955d937d767aaa7d1264ca
Alyssa <60706360+zobhhz@users.noreply.github.com>
2020-08-16 15:36:11 +0800

    Added counter
    Forgot to include sorry. I think the counters are in the right place but not really sure 😅

--- selection.c
+++ selection.c
@@ -9,22 +9,28 @@ void swap(int *xp, int *yp)
 	*yp = temp; 
 } 
 
-void selectionSort(int arr[], int n) 
+unsigned long long selectionSort(int arr[], int n) 
 { 
 	int i, j, min_idx; 
-
+	unsigned long long counter = 0;
+	
 	// One by one move boundary of unsorted subarray 
 	for (i = 0; i < n-1; i++) 
 	{ 
+		counter += 1;
 		// Find the minimum element in unsorted array 
 		min_idx = i; 
 		for (j = i+1; j < n; j++) 
-		if (arr[j] < arr[min_idx]) 
-			min_idx = j; 
+		{
+			counter += 1;
+			if (arr[j] < arr[min_idx]) 
+				min_idx = j; 
+		}
 
 		// Swap the found minimum element with the first element 
 		swap(&arr[min_idx], &arr[i]); 
 	} 
+	return counter;
 } 
 
 /* Function to print an array */
