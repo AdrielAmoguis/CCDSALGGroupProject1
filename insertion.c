7ac87541d48c1de268cb259d82179a61324d2184
Alyssa <60706360+zobhhz@users.noreply.github.com>
2020-08-16 15:43:40 +0800

    Added counter
    forgot to include sorry 😅

--- insertion.c
+++ insertion.c
@@ -3,10 +3,13 @@ Retrived from: https://www.geeksforgeeks.org/insertion-sort
 */
 
 /* Function to sort an array using insertion sort*/
-void insertionSort(int arr[], int n) 
+unsigned long long insertionSort(int arr[], int n) 
 { 
 	int i, key, j; 
+	unsigned long long counter = 0;
+	
 	for (i = 1; i < n; i++) { 
+		counter += 1;
 		key = arr[i]; 
 		j = i - 1; 
 
@@ -14,11 +17,13 @@ void insertionSort(int arr[], int n)
 		greater than key, to one position ahead 
 		of their current position */
 		while (j >= 0 && arr[j] > key) { 
+			counter += 1;
 			arr[j + 1] = arr[j]; 
 			j = j - 1; 
 		} 
 		arr[j + 1] = key; 
 	} 
+	return counter;
 } 
 
 // A utility function to print an array of size n 
